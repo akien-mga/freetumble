akien-mga/freetumble

commit 0b24e115a792c6fe02a53c82e114d298c9ee9cf3

Remove usage of getFrameTime(), and use sf::Clock instead
This might not be the best implementation, but it seems to work
when compared with V1.0 using SFML 1.5.

src/gameEngine/GameMain.cpp
@@ -37,7 +37,6 @@ GameMain::GameMain()
     loadConfig();
     buildMenus();
 
-    delay=0.0f;
     xSave=0;
     ySave=0;
 }
@@ -66,11 +65,11 @@ void GameMain::startGame()
         // time (menu = freeze time)
         if (activeMenu == menuNothing)
         {
-            gfxEngine->updateTime();
+            gfxEngine->updateEffects();
 
             if (!gameModel->getGameOver() && gameModel->getGameType() == GameModel::gameTypeMotion)
             {
-                gameModel->updateTime(gfxEngine->getFrameTime());
+                gameModel->updateTime(gfxEngine->getGameTime());
                 if (gameModel->getGameOver())
                 {
                     gameOver();
@@ -83,11 +82,10 @@ void GameMain::startGame()
         }
         if (state == stateGameOverAnim)
         {
-            delay-=gfxEngine->getFrameTime();
-            if (delay <= 0.0f)
+            if (gfxEngine->getGameOverTime() > gameOverAnimDelay)
             {
                 state=stateGameOver;
-                gfxEngine->setBestScoresDelay(bestScoresDelayMax);
+                gfxEngine->restartBestScoresClock();
                 if (gameModel->getGameType() != GameModel::gameTypeClassic)
                 {
                     gfxEngine->createGameOverEffect(gameModel);
@@ -415,7 +413,7 @@ void GameMain::click(int x, int y)
 void GameMain::gameOver()
 {
     state=stateGameOverAnim;
-    delay=gameOverAnimDelay;
+    gfxEngine->restartGameOverClock();
 
     gfxEngine->playMusic(GfxEngine::musicGameOver);
 
@@ -607,7 +605,7 @@ void GameMain::proceedInputGame()
                     gameScores->addScore(scoreGameType, gameModel->getScore(), scoreName);
                     gfxEngine->updateScores(gameScores, scoreGameType);
                     state=stateGameOver;
-                    gfxEngine->setBestScoresDelay(bestScoresDelayMax);
+                    gfxEngine->restartBestScoresClock();
                 }
                 break;
             }

src/gameEngine/GameModel.cpp
@@ -83,6 +83,7 @@ void GameModel::newGame(int gameType)
     overZone=false;
     gameOver=false;
     emptyZoneGrid();
+    oldTime=0.0f;
     delay=0.0f;
     rowDelay=delayForLevel[0];
     rowUp=false;
@@ -589,9 +590,10 @@ bool GameModel::canAddRow()
     return true;
 }
 
-void GameModel::updateTime(float dt)
+void GameModel::updateTime(float newTime)
 {
-    delay+=dt;
+    delay+=newTime - oldTime;
+    oldTime=newTime;
     if (delay > rowDelay)
     {
         delay-=rowDelay;

src/gameEngine/GameModel.h
@@ -130,6 +130,7 @@ private:
 
     bool gameOver;
 
+    float oldTime;
     float delay;
     float rowDelay;
 

src/contraints.h
@@ -66,7 +66,7 @@ const int stonesLevel = 120;
 
 const float upDelay = 0.25f;
 const float gameOverAnimDelay = 2.0f;
-const float bestScoresDelayMax = 1.0f;
+const float bestScoresDelay = 1.0f;
 //const float gameClickAnimDelay = 0.35f;
 
 const int SCORE_NAME_MAX = 12;

src/gfxEngine/EntityManager.cpp
@@ -21,6 +21,7 @@ EntityManager::EntityManager()
 {
     InitIterator();
     mItems = 0;
+    oldTime = 0.0f;
 }
 
 void EntityManager::Add(GameEntity* g)
@@ -45,8 +46,10 @@ GameEntity* EntityManager::NextItem()
     return e;
 }
 
-void EntityManager::Animate (float delay)
+void EntityManager::Animate (float newTime)
 {
+    float delay = newTime - oldTime;
+    oldTime = newTime;
     if (!IsEmpty())
     {
         EntityList::iterator it;

src/gfxEngine/EntityManager.h
@@ -45,7 +45,7 @@ private:
     EntityList EL;
     EntityList::iterator mIterator, mSavedIterator;
     int mItems;
-
+    float oldTime;
 };
 
 #endif

src/gfxEngine/GfxEngine.cpp
@@ -80,9 +80,6 @@ void GfxEngine::initialize()
     oldYMouse=0;
     initWindow();
     upAnimation=false;
-    delay=-10.0f;
-    bestScoresDelay=-10.0f;
-    consoleDelay=0.0f;
     consoleIndex=0;
     loadPictures();
     loadSounds();
@@ -516,12 +513,9 @@ void GfxEngine::renderIntro(int choice, Menu* myMenu)
 
 void GfxEngine::renderConsole()
 {
-    // FIXME: Find equivalent API
-    // consoleDelay += app->getFrameTime();
-    consoleDelay += 0.1f;
-    if (consoleDelay > CONSOLE_NEXT_LINE_DELAY)
+    if (getConsoleTime() > CONSOLE_NEXT_LINE_DELAY)
     {
-        consoleDelay=0.0f;
+        consoleClock.restart();
         consoleIndex++;
         if (consoleIndex >= CONSOLE_MAX_LINES) consoleIndex=0;
     }
@@ -535,10 +529,10 @@ void GfxEngine::renderConsole()
     for (int i=first; i <= consoleIndex; i++)
     {
 
-        if (i == consoleIndex && consoleDelay < CONSOLE_LINE_DELAY)
+        if (i == consoleIndex && getConsoleTime() < CONSOLE_LINE_DELAY)
         {
             int strSize=CONSOLE[i][language].length();
-            int n=strSize * consoleDelay/CONSOLE_LINE_DELAY;
+            int n=strSize * getConsoleTime()/CONSOLE_LINE_DELAY;
             menuString->setString(CONSOLE[i][language].substr(0, n));
         }
         else
@@ -794,9 +788,9 @@ void GfxEngine::renderTile(int x, int y, int mTile, float xTrans, float yTrans)
     int xPos=x * TILE_W + OFFSET_X + xTrans;
     int yPos=y * TILE_H + OFFSET_Y + yTrans;
 
-    if (delay > 0.0f)
+    if (getUpTime() <= upDelay)
     {
-        float fade=delay / upDelay;
+        float fade=getUpTime() / upDelay;
 
         yTrans+=OFFSET_Y * fade;
         yPos=y * TILE_H + OFFSET_Y + yTrans;
@@ -819,7 +813,7 @@ void GfxEngine::renderTile(int x, int y, int mTile, float xTrans, float yTrans)
     {
         effectsSprite.setPosition(xPos, yPos);
         effectsSprite.setTextureRect(sf::IntRect(0, 0, TILE_W, scaleY));
-        int alpha=128 + 127 * cosf(clock.getElapsedTime().asSeconds() * 8);
+        int alpha=128 + 127 * cosf(getGameTime() * 8);
         effectsSprite.setColor(sf::Color(255, 255, 255, alpha));
         app->draw(effectsSprite);
     }
@@ -827,7 +821,7 @@ void GfxEngine::renderTile(int x, int y, int mTile, float xTrans, float yTrans)
     {
         effectsSprite.setPosition(xPos, yPos);
         effectsSprite.setTextureRect(sf::IntRect(TILE_W, 0, TILE_W * 2, scaleY));
-        int alpha=128 + 127 * cosf(clock.getElapsedTime().asSeconds() * 9);
+        int alpha=128 + 127 * cosf(getGameTime() * 9);
         effectsSprite.setColor(sf::Color(255, 255, 255, alpha));
         app->draw(effectsSprite);
     }
@@ -835,7 +829,7 @@ void GfxEngine::renderTile(int x, int y, int mTile, float xTrans, float yTrans)
     {
         effectsSprite.setPosition(xPos, yPos);
         effectsSprite.setTextureRect(sf::IntRect(TILE_W * 2, 0, TILE_W * 3, scaleY));
-        int alpha=128 + 127 * cosf(clock.getElapsedTime().asSeconds() * 7);
+        int alpha=128 + 127 * cosf(getGameTime() * 7);
         effectsSprite.setColor(sf::Color(255, 255, 255, alpha));
         app->draw(effectsSprite);
     }
@@ -880,7 +874,7 @@ void GfxEngine::renderScores(GameModel* gameModel)
 void GfxEngine::renderDanger(GameModel* gameModel)
 {
     bool isInDanger=false;
-    if ((int)(clock.getElapsedTime().asSeconds() * 3) % 2 == 0) return;
+    if ((int)(getGameTime() * 3) % 2 == 0) return;
     for (int i=0; i < GRID_W; i++)
     {
         if (gameModel->getDanger(i))
@@ -942,9 +936,9 @@ void GfxEngine::renderGameOverBestScores(GameScores* gameScores, int gameType)
     int middle=MENU_POSX + MENU_W / 2;
 
     int yTrans=0;
-    if (bestScoresDelay > 0.0f)
+    if (getBestScoresTime() < bestScoresDelay)
     {
-        yTrans -= bestScoresDelay / bestScoresDelayMax * (MENU_TOP_H + MENU_SCORE_H * 10 + MENU_BUTTONL_H + MENU_BOTTOM_H + MENU_SPACE_H);
+        yTrans -= (bestScoresDelay - getBestScoresTime()) / bestScoresDelay * (MENU_TOP_H + MENU_SCORE_H * 10 + MENU_BUTTONL_H + MENU_BOTTOM_H + MENU_SPACE_H);
     }
 
     yTop += yTrans;
@@ -978,9 +972,9 @@ void GfxEngine::renderGameOverBestScores(GameScores* gameScores, int gameType)
 
         if (gameScores->getCurrentScore() % 10 == i)
         {
-            menuString->setColor(sf::Color(100 + sinf(clock.getElapsedTime().asSeconds() * 3) * 100,
-                                        100 + sinf(clock.getElapsedTime().asSeconds() * 4) * 100,
-                                        100 + cosf(clock.getElapsedTime().asSeconds() * 5) * 100));
+            menuString->setColor(sf::Color(100 + sinf(getGameTime() * 3) * 100,
+                                        100 + sinf(getGameTime() * 4) * 100,
+                                        100 + cosf(getGameTime() * 5) * 100));
         }
         else
         {
@@ -1113,7 +1107,7 @@ void GfxEngine::renderEnterName(GameModel* gameModel, string name)
 
     textScoreName->setString(string2wstring(name) + L"_");
     textScoreName->setPosition(middle - textScoreName->getLocalBounds().width / 2, yTop + MENU_TOP_H + 11);
-    if ((int)(clock.getElapsedTime().asSeconds() * 3) % 2 > 0) textScoreName->setString(string2wstring(name));
+    if ((int)(getGameTime() * 3) % 2 > 0) textScoreName->setString(string2wstring(name));
     app->draw(*textScoreName);
 
     //app->draw(blackFootSprite);
@@ -1228,7 +1222,7 @@ void GfxEngine::renderCredits()
     app->draw(*menuString);
 
     menuString->setCharacterSize(HELP_SUBTITLE_SIZE);
-    menuString->setColor(sf::Color(255, 255, 255, 128 + 127 * cos(5 * clock.getElapsedTime().asSeconds())));
+    menuString->setColor(sf::Color(255, 255, 255, 128 + 127 * cos(5 * getGameTime())));
     menuString->setString(CLICK_TO_CONTINUE[language]);
     menuString->setPosition((SCREEN_W - menuString->getLocalBounds().width) / 2, 510);
     app->draw(*menuString);
@@ -1320,7 +1314,7 @@ void GfxEngine::renderHelp()
     app->draw(*menuString);
 
     menuString->setCharacterSize(HELP_SUBTITLE_SIZE);
-    menuString->setColor(sf::Color(255, 255, 255, 128 + 127 * cos(5 * clock.getElapsedTime().asSeconds())));
+    menuString->setColor(sf::Color(255, 255, 255, 128 + 127 * cos(5 * getGameTime())));
     menuString->setString(CLICK_TO_CONTINUE[language]);
     menuString->setPosition((SCREEN_W - menuString->getLocalBounds().width) / 2, 525);
     app->draw(*menuString);
@@ -1401,34 +1395,15 @@ void GfxEngine::getKeys(GameInput* gameInput)
     }
 }
 
-void GfxEngine::updateTime()
-{
-    if (delay > 0.0f)
-    {
-        // FIXME: Find equivalent API
-        // delay -= app->getFrameTime();
-        delay -= 0.1f;
-    }
-    if (bestScoresDelay > 0.0f)
-    {
-        // FIXME: Find equivalent API
-        // bestScoresDelay -= app->getFrameTime();
-        bestScoresDelay -= 0.1f;
-    }
-    updateEffects();
-}
-
 void GfxEngine::updateEffects()
 {
-    // FIXME: Find equivalent API
-    // EM->Animate(app->getFrameTime());
-    EM->Animate(0.1f);
+    EM->Animate(getGameTime());
 }
 
 void GfxEngine::animateUp()
 {
     upAnimation=true;
-    delay=upDelay;
+    upClock.restart();
 }
 
 

src/gfxEngine/GfxEngine.h
@@ -40,12 +40,13 @@ public:
     ~GfxEngine();
 
     bool getStatusOK() { return statusOK; }
-    float getGameTime() { return clock.getElapsedTime().asSeconds(); }
-    float getFrameTime() {
-         // FIXME: Find a way to achieve that with a clock
-         // return app->getFrameTime();
-         return 0.1f;
-         }
+    float getGameTime() { return gameClock.getElapsedTime().asSeconds(); }
+    float getUpTime() { return upClock.getElapsedTime().asSeconds(); }
+    float getBestScoresTime() { return bestScoresClock.getElapsedTime().asSeconds(); }
+    float getGameOverTime() { return gameOverClock.getElapsedTime().asSeconds(); }
+    float getConsoleTime() { return consoleClock.getElapsedTime().asSeconds(); }
+    void restartBestScoresClock() { this->bestScoresClock.restart(); }
+    void restartGameOverClock() { this->gameOverClock.restart(); }
     bool getSoundEnabled() { return soundEnabled; }
     bool getMusicEnabled() { return musicEnabled; }
     bool getEffectsEnabled() { return effectsEnabled; }
@@ -70,7 +71,6 @@ public:
     void setMouseOnLeftButton(bool mouseOnLeftButton) { this->mouseOnLeftButton=mouseOnLeftButton; }
     void setMouseOnRightButton(bool mouseOnRightButton) { this->mouseOnRightButton=mouseOnRightButton; }
     void setMouseOnCancelButton(bool mouseOnCancelButton) { this->mouseOnCancelButton=mouseOnCancelButton; }
-    void setBestScoresDelay(float bestScoresDelay) { this->bestScoresDelay=bestScoresDelay; }
     void setIntroMouseOver(int introMouseOver) { this->introMouseOver=introMouseOver; }
 
     // rendering methodes
@@ -95,7 +95,6 @@ public:
     void renderHelp();
 
     void getKeys(GameInput* gameInput);
-    void updateTime();
     void updateEffects();
     void animateUp();
 
@@ -290,11 +289,12 @@ private:
     float xRand;
     float yRand;
 
-    sf::Clock clock;
+    sf::Clock gameClock;
+    sf::Clock upClock;
+    sf::Clock bestScoresClock;
+    sf::Clock consoleClock;
+    sf::Clock gameOverClock;
 
-    float delay;
-    float bestScoresDelay;
-    float consoleDelay;
     int consoleIndex;
     bool upAnimation;
 

commit 9db332d74fd69381022b06398bcec631df71cb9b

Initialise timer when motion game starts
A regression introduced in 0b24e11 made the motion game type
depend on the absolute uptime of the game instead of using its
own delta. This is now used by reintroducing a similar logic to
that of V1.0.

src/gameEngine/GameMain.cpp
@@ -69,7 +69,9 @@ void GameMain::startGame()
 
             if (!gameModel->getGameOver() && gameModel->getGameType() == GameModel::gameTypeMotion)
             {
-                gameModel->updateTime(gfxEngine->getGameTime());
+                float newTime=gfxEngine->getGameTime();
+                gameModel->updateTime(newTime - oldTime);
+                oldTime=newTime;
                 if (gameModel->getGameOver())
                 {
                     gameOver();
@@ -172,6 +174,7 @@ void  GameMain::newGame() {
             gameModel->newGame(GameModel::gameTypeMotion);
             gameModel->setHighScore(gameScores->getMotionScore(0).score);
             gfxEngine->playMusic(GfxEngine::musicGame);
+            oldTime=gfxEngine->getGameTime();
             break;
         }
         case choiceClassic:

src/gameEngine/GameMain.h
@@ -94,6 +94,7 @@ private:
     int activeMenu;
 
     float delay;
+    float oldTime;
 
     void newGame();
     void moveInMenu(int x, int y);

src/gameEngine/GameModel.cpp
@@ -83,7 +83,6 @@ void GameModel::newGame(int gameType)
     overZone=false;
     gameOver=false;
     emptyZoneGrid();
-    oldTime=0.0f;
     delay=0.0f;
     rowDelay=delayForLevel[0];
     rowUp=false;
@@ -590,10 +589,9 @@ bool GameModel::canAddRow()
     return true;
 }
 
-void GameModel::updateTime(float newTime)
+void GameModel::updateTime(float dt)
 {
-    delay+=newTime - oldTime;
-    oldTime=newTime;
+    delay+=dt;
     if (delay > rowDelay)
     {
         delay-=rowDelay;

src/gameEngine/GameModel.h
@@ -130,7 +130,6 @@ private:
 
     bool gameOver;
 
-    float oldTime;
     float delay;
     float rowDelay;
 